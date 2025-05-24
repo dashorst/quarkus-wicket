f1d982ff9feadc2d2855a1e56c5eca2ba7b668ad
Martijn Dashorst <martijn.dashorst@topicus.nl>
2025-05-24 18:39:01 +0200

    fix: make the WicketProcessor build step work

--- deployment/src/main/java/io/quarkiverse/wicket/deployment/WicketProcessor.java
+++ deployment/src/main/java/io/quarkiverse/wicket/deployment/WicketProcessor.java
@@ -1,17 +1,12 @@
 package io.quarkiverse.wicket.deployment;
 
-import java.io.IOException;
-
-import io.quarkiverse.wicket.runtime.config.WicketConfiguration;
-import io.quarkus.deployment.annotations.BuildProducer;
-import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.builditem.FeatureBuildItem;
-import io.quarkus.deployment.builditem.GeneratedResourceBuildItem;
-import io.quarkus.vertx.http.deployment.spi.GeneratedStaticResourceBuildItem;
 import jakarta.inject.Inject;
 
-class WicketProcessor {
+import io.quarkiverse.wicket.runtime.config.WicketConfiguration;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
 
+class WicketProcessor {
     private static final String FEATURE = "wicket";
 
     @Inject
@@ -20,11 +15,5 @@ class WicketProcessor {
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
-    }
-
-    @BuildStep
-    void generateApplicationClass(BuildProducer<GeneratedStaticResourceBuildItem> generatedStaticResourceProducer,
-    BuildProducer<GeneratedResourceBuildItem> generatedResourceProducer) throws IOException{
-        generatedResourceProducer.produce(new GeneratedResourceBuildItem());;
     }
 }
