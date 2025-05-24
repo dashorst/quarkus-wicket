2da5bcf594120b1ad482a9236262eb3c8937f42c
Martijn Dashorst <martijn.dashorst@topicus.nl>
2025-05-24 18:38:38 +0200

    fix: configure QuarkusWicketFilter for CDI application
    In order to use CDI for bootstrapping Wicket and the Wicket filter, we
    need to inject the application into the filter.
    
    To ensure that the filter's configuring of the application happens on
    a single instance of the Wicket application, the application object must
    be a `@Singleton`.

--- runtime/src/main/codestarts/quarkus/wicket-codestart/java/src/main/java/org/acme/MyApplication.java
+++ runtime/src/main/codestarts/quarkus/wicket-codestart/java/src/main/java/org/acme/MyApplication.java
@@ -1,10 +1,17 @@
 package org.acme;
 
-import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Singleton;
 import io.quarkiverse.wicket.runtime.QuarkusWicketApplication;
 
-@ApplicationScoped
+@Singleton
 public class MyApplication extends QuarkusWicketApplication {
+    @Override
+    protected void init() {
+        super.init();
+
+        // Add your configuration here
+    }
+
     @Override
     public Class<? extends org.apache.wicket.Page> getHomePage() {
         return MyPage.class;
---
+++ runtime/src/main/java/io/quarkiverse/wicket/runtime/QuarkusWicketFilter.java
@@ -0,0 +1,12 @@
+package io.quarkiverse.wicket.runtime;
+
+import jakarta.inject.Inject;
+
+import org.apache.wicket.protocol.http.WicketFilter;
+
+public class QuarkusWicketFilter extends WicketFilter {
+    @Inject
+    public QuarkusWicketFilter(QuarkusWicketApplication application) {
+        super(application);
+    }
+}
